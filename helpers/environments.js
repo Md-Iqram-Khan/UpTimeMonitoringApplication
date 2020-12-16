28a6231ee352677231eddf68100907f8116c7c0c
Md-Iqram-Khan <you@example.com>
2020-12-16 19:10:36 +0600

    Implement Check

--- helpers/environments.js
+++ helpers/environments.js
@@ -13,13 +13,15 @@
  environments.staging = {
        port : 3000,
        envName : 'staging',
-       secretKey : 'hdsjfhj'
+       secretKey : 'hdsjfhj',
+       maxChecks : 5
  }
 
  environments.production = {
       port : 5000,
       envName : 'production',
-      secretKey : 'rewtuhio'
+      secretKey : 'rewtuhio',
+      maxChecks : 5
 }
 
 //determined which environment was passed 
